1fd0916e0f0ffb82617921fe7a48125d7d5ed9aa
TartCherryVentures <samwilliam223@gmail.com>
2025-08-19 22:51:28 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ export default {
     extend: {
       colors: {
         navy: {
-          900: '#0f172a',
+          50:  '#f1f5f9',
+          100: '#e2e8f0',
+          200: '#cbd5e1',
+          300: '#94a3b8',
+          400: '#64748b',
+          500: '#334155',   // use this as your main
+          600: '#1e293b',
+          700: '#1e293b',
+          800: '#0f172a',
+          900: '#0a1623'
         }
       },
       animation: {
-        'fadeIn': 'fadeIn 0.3s ease-in-out',
+        fadeIn: 'fadeIn 0.3s ease-in-out',
       },
       keyframes: {
         fadeIn: {
