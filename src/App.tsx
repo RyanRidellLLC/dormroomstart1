5698f89f8f88157aaaadc3f905b0636b02a15c32
TartCherryVentures <samwilliam223@gmail.com>
2025-08-19 23:53:33 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import StartupSignup from "./pages/StartupSignup";
+import InvestorSignup from "./pages/InvestorSignup";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup/startup" element={<StartupSignup />} />
+        <Route path="/signup/investor" element={<InvestorSignup />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
